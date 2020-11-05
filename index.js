0177e3e1f46439f258ca910f8317cf93b5ede3ec
Garvit Shrestha <shrestha.garvit@wensolutions.com>
2020-11-05 19:39:42 +0545

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * A javascript library for WordPress themes to help the theme developers with theme accessibility part.
+ *
+ * @author garvitshrestha
+ * @version 1.0.0
+ * @source https://github.com/garvitshrestha/wp-accessibility-js
+ * @license GPL-3.0 https://github.com/garvitshrestha/wp-accessibility-js/blob/master/LICENSE
+ */
+
+const wpAccessibilityJS = () => {
+    
+}
